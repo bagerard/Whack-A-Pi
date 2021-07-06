d0866820571177c42ad6a182a6954e44966128a6
Bastien Gerard <bastien.gerard@cluepoints.com>
2021-07-06 22:29:30 +0200

    reworking

--- main.py
+++ main.py
@@ -1,120 +1,169 @@
-import time, pygame, sys, os, json
-from pygame.locals import *
+import time
+import os
+import json
+
+import pygame
+import sys
+
 from screens import *
 import react
+import traceback
 
-mode = "menu"
 game_level = -1
-high_scores = [[0, "Olivier", "Athos",None,None],
-				[0, "Rene", "Aramis",None,None],
-				[0, "Isaac", "Porthos",None,None]]
 
-gameengine = react.Game()
-filename = "scores.json"
-	
-def save_scores():
-	if os.access(filename, os.W_OK):
-		os.rename(filename, filename + "." + str(time.time()))
-	with open(filename, mode='w', encoding='utf-8') as f:
-		json.dump(high_scores, f, indent=2)
+DEFAULT_HIGH_SCORES = [
+    [0, "Olivier", "Athos", None, None],
+    [0, "Rene", "Aramis", None, None],
+    [0, "Isaac", "Porthos", None, None],
+]
+
+SCORE_FILE = "scores.json"
+
+
+class GameContext:
+    def __init__(self, current_mode):
+        self.current_mode = current_mode
+
+
+def save_scores(high_scores):
+    print("save_score")
+    if os.access(SCORE_FILE, os.W_OK):
+        os.rename(SCORE_FILE, SCORE_FILE + "." + str(time.time()))
+    with open(SCORE_FILE, mode="w", encoding="utf-8") as f:
+        json.dump(high_scores, f, indent=2)
+
 
 def load_scores():
-	global high_scores
-	if os.access(filename, os.R_OK):
-		print("open")
-		with open(filename, 'r', encoding='utf-8') as f:
-			high_scores = json.load(f)
-	
-def on_click():
-	global game_level, mode
-	click_pos = (pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
-	
-	if mode == "menu":
-		button_width = SIZE[0]/3
-		if click_pos[1] > SIZE[1]/3:
-			game_level = int(click_pos[0]/button_width)
-			mode = "initgame"
-	elif mode == "postgame":
-		mode = "menu"
-		show_mainscreen()
-		
-def show_mainscreen():
-	gameengine.start_idle()
-	menu_screen(screen, high_scores)
-		
-		
+    print("load_score")
+    if os.access(SCORE_FILE, os.R_OK):
+        print("open")
+        with open(SCORE_FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+    print('Load default scores')
+    return DEFAULT_HIGH_SCORES
+
+
+def on_click(game_engine, high_scores, game_ctx):
+    print("on_click")
+    global game_level
+    click_pos = (pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+
+    if game_ctx.current_mode == "menu":
+        button_width = SIZE[0] / 3
+        if click_pos[1] > SIZE[1] / 3:
+            game_level = int(click_pos[0] / button_width)
+            game_ctx.current_mode = "initgame"
+    elif game_ctx.current_mode == "postgame":
+        game_ctx.current_mode = "menu"
+        show_mainscreen(game_engine, high_scores)
+
+
+def show_mainscreen(game_engine, high_scores) -> None:
+    print("show_mainscreen")
+    game_engine.start_idle()
+    print("menu_screen")
+    menu_screen(screen, high_scores)
+    print("out of show_mainscreen")
+
+
+def play_music(mp3_path: str) -> None:
+    # pygame.mixer.music.load(mp3_path)
+    # pygame.mixer.music.play()
+    print(f'fake play music {mp3_path}')
+
+
 def main():
-	global mode
-	load_scores()
-	show_mainscreen()
-	while True:
-		if mode == "initgame":
-			gameengine.stop_idle()
-			game_screen(screen,60, 0, high_scores[game_level][0], True)
-			if gameengine.ready_wait(30):
-				pygame.mixer.music.load("Robot Wars Clean SFX- 3 2 1 Actvate!.mp3")
-				pygame.mixer.music.play()
-				while pygame.mixer.music.get_busy():
-					time.sleep(0.1)
+    game_ctx = GameContext(current_mode="menu")
+    game_engine = react.Game()
+    print("main")
+    high_scores = load_scores()
+    show_mainscreen(game_engine, high_scores=high_scores)
 
-				elapsed = 61
-				score = 0
-				last_elapsed = -1
-				last_score = -1
-				pygame.mixer.music.load("mi.mp3")
-				pygame.mixer.music.play()
-				mode = "game"
-				gameengine.start_game()
-			else:
-				mode = "menu"
-				show_mainscreen()
+    print('start main while loop')
+    prev_mode = game_ctx.current_mode
+    while True:
+        if prev_mode != game_ctx.current_mode:
+            print("SWITCH TO MODE", game_ctx.current_mode)
+            prev_mode = game_ctx.current_mode
 
-		elif mode == "game":
-			elapsed = int(round(60 - gameengine.elapsed_time(),0))
-			score = gameengine.score
-			if elapsed < 0:
-				game_screen(screen,0, score, high_scores[game_level][0])
-				pygame.mixer.music.load("airhorn.mp3")
-				pygame.mixer.music.play()
-				while pygame.mixer.music.get_busy():
-					time.sleep(0.1)
-				if score > high_scores[game_level][0]:
-					ret = win_screen(screen)
-					high_scores[game_level][0] = score
-					high_scores[game_level][1] = ret[0]
-					high_scores[game_level][2] = ret[1]
-					high_scores[game_level][3] = ret[2]
-					high_scores[game_level][4] = ret[3]
-					save_scores()
-					mode = "menu"
-					show_mainscreen()
-				else:
-					lose_screen(screen)
-					mode = "postgame"
-				
-			elif elapsed != last_elapsed or score != last_score:
-				game_screen(screen,elapsed, score, high_scores[game_level][0])
-				last_elapsed = elapsed
-				last_score = score
-			
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit() 
-				sys.exit()
-			if event.type == KEYDOWN:
-				if event.key == K_F12:
-					gameengine.stop_idle()
-					gameengine.stop_game()
-					pygame.quit()
-					sys.exit() 
-			if event.type == pygame.MOUSEBUTTONUP:
-					pos = (pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
-					#pygame.draw.circle(screen, white, pos, 2, 0) #for debugging purposes - adds a small dot where the screen is pressed
-					on_click()					
+        if game_ctx.current_mode == "initgame":
+            game_engine.stop_idle()
+            print('game_screen')
+            game_screen(screen, 60, 0, high_scores[game_level][0], True)
+
+            game_engine.start_loop_btn_thread()                         # BAG DEBUG
+            print('game_engine.ready_wait')
+            if game_engine.ready_wait(60):
+                play_music("Robot Wars Clean SFX- 3 2 1 Actvate!.mp3")
+                while pygame.mixer.music.get_busy():
+                    time.sleep(0.1)
+
+                elapsed = 61
+                score = 0
+                last_elapsed = -1
+                last_score = -1
+                play_music("mi.mp3")
+                game_ctx.current_mode = "game"
+                game_engine.start_game()
+            else:
+                print('! if game_engine.ready_wait(30):')
+                game_ctx.current_mode = "menu"
+                show_mainscreen(game_engine, high_scores=high_scores)
+
+        elif game_ctx.current_mode == "game":
+            elapsed = int(round(60 - game_engine.elapsed_time(), 0))
+            score = game_engine.score
+            if elapsed < 0:
+                game_screen(screen, 0, score, high_scores[game_level][0])
+                play_music("airhorn.mp3")
+                while pygame.mixer.music.get_busy():
+                    time.sleep(0.1)
+                if score > high_scores[game_level][0]:
+                    ret = win_screen(screen)
+                    high_scores[game_level][0] = score
+                    high_scores[game_level][1] = ret[0]
+                    high_scores[game_level][2] = ret[1]
+                    high_scores[game_level][3] = ret[2]
+                    high_scores[game_level][4] = ret[3]
+                    save_scores(high_scores)
+                    game_ctx.current_mode = "menu"
+                    show_mainscreen(game_engine, high_scores=high_scores)
+                else:
+                    lose_screen(screen)
+                    game_ctx.current_mode = "postgame"
+
+            elif elapsed != last_elapsed or score != last_score:
+                game_screen(screen, elapsed, score, high_scores[game_level][0])
+                last_elapsed = elapsed
+                last_score = score
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                print(f'Event: QUIT')
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                print(f'Event: KEYDOWN')
+                if event.key == pygame.K_F12:
+                    print(f'Event: K_F12')
+                    game_engine.stop_idle()
+                    game_engine.stop_game()
+                    pygame.quit()
+                    sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                print(f'Event: MOUSEBUTTONUP')
+                pos = (pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+                pygame.draw.circle(screen, pygame.Color(255, 10, 10), pos, 2, 0) #for debugging purposes - adds a small dot where the screen is pressed
+                on_click(game_engine, high_scores, game_ctx)
+
+
 try:
-	pygame.init()
-	screen = pygame.display.set_mode(SIZE)
-	main()
-except:
-	pygame.quit()
-	sys.exit()
+    pygame.init()
+    screen = pygame.display.set_mode(SIZE)
+
+    main()
+except BaseException as e:
+    traceback.print_exception(*sys.exc_info())
+    pygame.quit()
+    sys.exit()
--- react.py
+++ react.py
@@ -3,154 +3,202 @@ import time
 from random import randint
 from threading import Thread, Event
 
-class Game():
-	def __init__(self):
-		self.lights = LEDBoard(2,3,4,14,15,17,18,27,22,23,24,10,9)
-		self.buttons = [Button(25), Button(11), Button(8), Button(7)
-						,Button(5), Button(6), Button(12), Button(13)
-						,Button(19), Button(16), Button(26), Button(20)
-						,Button(21)]
-		self.starttime = 0
-		self.score = 0
-		self.gamethread = None
-		self.idlethread = None
-		self.max_leds = len(self.lights.leds)
-		self.idle_stop = Event()
-		self.game_stop = Event()
-		self.current_idx = -1		
-		
-	def button_test(self):
-		self.lights.on()
-		time.sleep(5)
-		self.lights.off()
 
-		for idx in range(0,self.max_leds):
-			print (self.lights.leds[idx].pin.number)
-			self.lights.leds[idx].on()
-			self.buttons[idx].wait_for_press(30)
-			self.lights.leds[idx].off()
-			
-	def elapsed_time(self):
-		return time.time() - self.starttime
+class Game:
+    def __init__(self):
+        self.lights = LEDBoard(2, 3, 4, 14, 15, 17, 18, 27, 22, 23, 24, 10, 9)
+        self.buttons = [
+            Button(25),
+            Button(11),
+            Button(8),
+            Button(7),
+            Button(5),
+            Button(6),
+            Button(12),
+            Button(13),
+            Button(19),
+            Button(16),
+            Button(26),
+            Button(20),
+            Button(21),
+        ]
+        self.start_time = 0
+        self.score = 0
+        self.game_thread = None
+        self.idle_thread = None
+        self.idle_stop = Event()
+        self.game_stop = Event()
+        self.current_idx = -1
 
-	def wait_for_press(self, idx, delay = -1):
-		wait_start = time.time()
-		while not self.game_stop.isSet():
-			if self.buttons[idx].is_pressed:
-				return True
-			if time.time() - wait_start >= delay:
-				break
-		return False
+    def start_loop_btn_thread(self):
+        def _loop_btn(buttons):
+            while True:
+                for btn in buttons:
+                    print(f'push button {btn}')
+                    btn.pin.drive_low()
+                    print(f'release button {btn}')
+                    btn.pin.drive_high()
+                    time.sleep(1)
+        print('start start_loop_btn_thread')
+        Thread(target=_loop_btn, args=(self.buttons,)).start()
 
-	def ready_wait(self, delay):
-		self.lights.off()
-		idx = 3
-		self.lights.leds[idx].on()
-		self.buttons[idx].wait_for_press(delay)
-		self.lights.leds[idx].off()
-		return self.buttons[idx].is_pressed
-		
-		
-	def start_game(self):
-		self.game_stop.clear()
-		self.gamethread = Thread(target=self._rungame)
-		self.starttime = time.time()
-		self.gamethread.start()
+    @property
+    def n_leds(self):
+        return len(self.lights.leds)
 
-	def stop_game(self):
-		self.buttons[self.current_idx].close()
-		self.game_stop.set()
-		self.gamethread.join()
+    def button_test(self):
+        print("button test")
+        self.lights.on()
+        time.sleep(5)
+        self.lights.off()
 
-	def _rungame(self):
-		elapsed = 0
-		last_idx = -1
-		self.score = 0
-		while elapsed < 60 and not self.game_stop.isSet():
-			while last_idx == self.current_idx:
-				self.current_idx = randint(0,self.max_leds - 1)
-			self.lights.leds[self.current_idx].on()
-			delay = max(0, 60 - self.elapsed_time())
-			#print(delay)
-			self.buttons[self.current_idx].wait_for_press(delay)
-			if self.buttons[self.current_idx].is_pressed:
-				self.score += 1
-			self.lights.leds[self.current_idx].off()
-			last_idx = self.current_idx
-			elapsed = self.elapsed_time()
-		#print ("Time up")
-		#print("Score:", score)
+        for idx in range(0, self.n_leds):
+            print(self.lights.leds[idx].pin.number)
+            self.lights.leds[idx].on()
+            self.buttons[idx].wait_for_press(30)
+            self.lights.leds[idx].off()
 
-	def start_idle(self):
-		self.idle_stop.clear()
-		self.idlethread = Thread(target=self._idle)
-		self.idlethread.start()
-		
-	def stop_idle(self):
-		self.idle_stop.set()
-		self.idlethread.join()
-	
-	def _idle(self):
-		max_patterns = 4
-		rows = [[0,1], [2,3,4], [5,6,7,8], [9,10,11],[12]]
-		cols = [[5], [0,2,9], [6], [3,10,12], [7], [1,4,11], [8]]
-		pulse = [[6,7], [3,10], [2,4,9,11], [5,8], [0,1,12]]
+    def elapsed_time(self):
+        return time.time() - self.start_time
 
-		idle_pattern = 3
-		while not self.idle_stop.isSet():
-			idle_pattern = randint(0,max_patterns)
-			#idle_pattern += 1
-			if idle_pattern > max_patterns:
-				break
-				
-			self.lights.off()
-			seq_start = time.time()
+    def wait_for_press(self, idx, delay=-1):
+        print("wait_for_press")
+        wait_start = time.time()
+        while not self.game_stop.isSet():
+            if self.buttons[idx].is_pressed:
+                return True
+            if time.time() - wait_start >= delay:
+                break
+        return False
 
-			if idle_pattern == 0:
-				while time.time() - seq_start < 5 and not self.idle_stop.isSet():
-					self.lights.leds[randint(0,self.max_leds - 1)].toggle()
-					self.idle_stop.wait(0.1)
-					
-			if idle_pattern == 1:
-				for i in range(0,2):
-					count = 0
-					while count < self.max_leds and not self.idle_stop.isSet():
-						self.lights.leds[count].toggle()
-						count += 1
-						self.idle_stop.wait(0.25)
+    def ready_wait(self, delay):
+        print('ready_wait')
+        self.lights.off()
+        btn_idx = 3
+        led = self.lights.leds[btn_idx]
+        btn = self.buttons[btn_idx]
 
-			if idle_pattern == 2:
-				self.lights.blink(on_time=0.5, off_time=0.5, background = True)
-				while time.time() - seq_start < 5 and not self.idle_stop.isSet():
-					pass 
-				self.lights.off()
-				
-			if idle_pattern == 3:
-				delay = 0.5
-				for i in range (0,2):
-					for r in rows:
-						for idx in r:
-							self.lights.leds[idx].on()
-						self.idle_stop.wait(delay)					
-						for idx in r:
-							self.lights.leds[idx].off()
+        print(f'Light on {led}')
+        led.on()
+        print(f'Waiting for {btn}...')
+        btn_pressed = btn.wait_for_press(delay)
 
-					if not self.idle_stop.isSet():
-						for c in cols:
-							for idx in c:
-								self.lights.leds[idx].on()
-							self.idle_stop.wait(delay)					
-							for idx in c:
-								self.lights.leds[idx].off()
+        led.off()
+        print('btn_pressed')
+        return btn_pressed
 
-			if idle_pattern == 4:
-				delay = 0.5
-				for i in range (0,2):
-					for group in pulse:
-						for idx in group:
-							self.lights.leds[idx].on()
-						self.idle_stop.wait(delay)					
-						for idx in group:
-							self.lights.leds[idx].off()
+    def start_game(self):
+        print("start_game")
+        self.game_stop.clear()
+        self.game_thread = Thread(target=self._rungame)
+        self.start_time = time.time()
+        self.game_thread.start()
 
-		self.lights.off()			
+    def stop_game(self):
+        print("stop_game")
+        self.buttons[self.current_idx].close()
+        self.game_stop.set()
+        self.game_thread.join()
+
+    def _rungame(self):
+        print("_rungame")
+        elapsed = 0
+        last_idx = -1
+        self.score = 0
+        while elapsed < 60 and not self.game_stop.isSet():
+            # Find a button that is not the last one
+            while last_idx == self.current_idx:
+                self.current_idx = randint(0, self.n_leds - 1)
+
+            led = self.lights.leds[self.current_idx]
+            btn = self.buttons[self.current_idx]
+
+            led.on()
+
+            delay = max(0, 60 - self.elapsed_time())
+            # print(delay)
+
+            print(f'run game, waiting on {btn}')
+            if btn.wait_for_press(delay):
+                self.score += 1
+
+            led.off()
+
+            last_idx = self.current_idx
+            elapsed = self.elapsed_time()
+        # print ("Time up")
+        # print("Score:", score)
+
+    def start_idle(self):
+        self.idle_stop.clear()
+        self.idle_thread = Thread(target=self._idle)
+        self.idle_thread.start()
+
+    def stop_idle(self):
+        self.idle_stop.set()
+        self.idle_thread.join()
+
+    def _idle(self):
+        max_patterns = 4
+        rows = [[0, 1], [2, 3, 4], [5, 6, 7, 8], [9, 10, 11], [12]]
+        cols = [[5], [0, 2, 9], [6], [3, 10, 12], [7], [1, 4, 11], [8]]
+        pulse = [[6, 7], [3, 10], [2, 4, 9, 11], [5, 8], [0, 1, 12]]
+
+        idle_pattern = 3
+        while not self.idle_stop.isSet():
+            idle_pattern = randint(0, max_patterns)
+            # idle_pattern += 1
+            if idle_pattern > max_patterns:
+                break
+
+            self.lights.off()
+            seq_start = time.time()
+
+            if idle_pattern == 0:
+                while time.time() - seq_start < 5 and not self.idle_stop.isSet():
+                    self.lights.leds[randint(0, self.n_leds - 1)].toggle()
+                    self.idle_stop.wait(0.1)
+
+            if idle_pattern == 1:
+                for i in range(0, 2):
+                    count = 0
+                    while count < self.n_leds and not self.idle_stop.isSet():
+                        self.lights.leds[count].toggle()
+                        count += 1
+                        self.idle_stop.wait(0.25)
+
+            if idle_pattern == 2:
+                self.lights.blink(on_time=0.5, off_time=0.5, background=True)
+                while time.time() - seq_start < 5 and not self.idle_stop.isSet():
+                    pass
+                self.lights.off()
+
+            if idle_pattern == 3:
+                delay = 0.5
+                for i in range(0, 2):
+                    for r in rows:
+                        for idx in r:
+                            self.lights.leds[idx].on()
+                        self.idle_stop.wait(delay)
+                        for idx in r:
+                            self.lights.leds[idx].off()
+
+                    if not self.idle_stop.isSet():
+                        for c in cols:
+                            for idx in c:
+                                self.lights.leds[idx].on()
+                            self.idle_stop.wait(delay)
+                            for idx in c:
+                                self.lights.leds[idx].off()
+
+            if idle_pattern == 4:
+                delay = 0.5
+                for i in range(0, 2):
+                    for group in pulse:
+                        for idx in group:
+                            self.lights.leds[idx].on()
+                        self.idle_stop.wait(delay)
+                        for idx in group:
+                            self.lights.leds[idx].off()
+
+        self.lights.off()
